76c74a7df8153defe6ab9de44c30f98371fb911a
Anushka11117 <21ee01070@iitbbs.ac.in>
2024-12-20 01:11:35 +0530

    Staged all changes for the project

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,6 @@
 const userModel=require("../models/user-model")
-  const bcrypt= require("bcrypt");
-
+const  {comparePass } = require("../utils/decryptPass");
+const { hashPass } = require("../utils/encryptPass");
 const { generateTokens } = require('../utils/generate token');
 
 
@@ -12,30 +12,57 @@ try {
 let{fullname, email, password}=req.body;
 
 
-let userc= await userModel.findOne({email:email});
-if(userc)return res.status(401).send("you already have an account");
+let ifUser= await userModel.findOne({email:email});
+if (ifUser) {     
+    // req.flash("message" , "This email is already registered");
+    // res.redirect("/");
+    res.send("already registered")
+  }
 
-
-let salt= await bcrypt.genSalt(10);
-let hashed_Password= await bcrypt.hash(password, salt);
-
+  const hashedPass = await hashPass(password);
 
     let user = await userModel.create({
         email,
-        password:hashed_Password,
+        password:hashedPass,
         fullname
 
     })
 
-    let token=generateTokens(user);
+    const token=generateTokens(user);
     res.cookie("token",token);
-    res.send("user registered successfully");
-    
-
-
-} catch (error) {
-    console.log(error.message)
+    // req.flash("message" , "Account created, you can login now");
+    // res.redirect("/");
+    res.send("created")
+} 
+catch (error) { res.send(error.message);}
 }
 
 
-}
+  module.exports.loginUser =async (req, res) => {
+    try {
+      let { email, password } = req.body;
+      let ifUser = await userModel.findOne({ email: email });
+      if (!ifUser) {
+        //  req.flash("message" ,"Email or password is incorrect");
+        //  res.redirect('/')
+        res.send("create first")
+      }
+       else {
+        let user = ifUser;
+        let isMatch = await comparePass(password , user.password);
+       if (!isMatch) {
+        // req.flash("message" ,"Email or password is incorrect");
+        // res.redirect('/')
+       }
+       if(isMatch) {
+         const token = generateTokens(user);
+         res.cookie("token" , token)
+        //  console.log("redirecting")
+        //  res.redirect("/shop")
+        res.send('you can login')
+         }
+      }
+    } catch (error) {
+      res.send(error.message);
+    }
+  };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,11 @@
 const express=require ('express');
 const router =express.Router();
-const {registerUser}=require("../Controllers/authController")
+const {registerUser,loginUser}=require("../Controllers/authController")
 
 router.get("/",function(req,res){
     res.send("its working");
 })
 router.post("/register",registerUser );
+router.post('/login' , loginUser)
 
 module.exports=router;
